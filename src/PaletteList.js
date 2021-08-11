7cdf9f42c0a2e2af027ab3479e7868f2a708458b
arryan <arryan@umich.edu>
2021-08-10 21:34:12 -0400

    change title to Palette Splash

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -45,7 +45,7 @@ class PaletteList extends Component {
             <div className = {classes.root}>
                 <div className = {classes.container}>
                     <nav className = {classes.nav}>
-                        <h1 className = {classes.heading}>React Colors</h1>
+                        <h1 className = {classes.heading}>Palette Splash</h1>
                         <Link to = "/palette/new">Create Palette</Link>
                     </nav>
                         <TransitionGroup className ={classes.palettes}>
